291d333056765fc9c709d8a6e721d102d53782c3
Lyushen <78591028+Lyushen@users.noreply.github.com>
2023-11-28 15:39:52 +0000

    Day Eight

--- AllInOneApp.cs
+++ AllInOneApp.cs
@@ -6,6 +6,7 @@ using System.ComponentModel;
 using System.Data.Common;
 using System.Text;
 using System.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 
 internal class Program
@@ -52,13 +53,97 @@ internal class Program
      // Program.DayFour();
      // Program.DayFive();
      // Program.DaySix();
-        Program.DaySeven();
+     // Program.DaySeven();
+        Program.DayEight();
 
         stopwatch.Stop();
         Console.WriteLine($"\nPress any key to exit...\tProcessing time: {stopwatch.ElapsedMilliseconds} ms");
         Console.ReadKey();
         Environment.Exit(0);
     }
+
+    static void DayEight()
+    {
+        //ComeBackToDaySeven();
+        ReWriteCSV();
+
+        static void ReWriteCSV()
+        {
+            const int IDX_FN = 0;
+            const int IDX_LN = 1;
+            const int IDX_AGE = 2;
+            const int IDX_GROUP = 3;
+            const int IDX_EMPLOYER = 4;
+
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\";
+            string csvFilePath = desktopPath + @"SampleCSV.csv";
+            try
+            {
+                string[]? dataLines = new string[] { };
+                dataLines = File.ReadAllLines(csvFilePath);
+                if (dataLines.Length == 0 ) {
+                    throw new Exception($"File '{csvFilePath}' does not contain data");
+                }
+                string[]? apprentices = dataLines.Skip(1).ToArray();
+                string[] headers = dataLines[0].Split(',');
+                dataLines = null;
+                int numberApprentices = apprentices.Length;
+                string[] firstName = new string[numberApprentices];
+                string[] lastName = new string[numberApprentices];
+                int[] apprentAge = new int[numberApprentices];
+                string[] group = new string[numberApprentices];
+                string[] employer = new string[numberApprentices];
+
+                for (int i = 0; i < numberApprentices; i++) // skip the headers starting from index 1
+                {
+                    string[] dataFields = apprentices[i].Split(',');
+                    apprentAge[i] = Convert.ToInt32(dataFields[IDX_AGE]);
+                    firstName[i] = dataFields[IDX_FN];
+                    lastName[i] = dataFields[IDX_LN];
+                    group[i] = dataFields[IDX_GROUP];
+                    employer[i] = dataFields[IDX_EMPLOYER];
+                }
+                DisplayAgeInfo(firstName, lastName, apprentAge);
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error:\n{ex.Message}");
+            }
+            
+            static void DisplayAgeInfo(string[] firstName, string[] lastName,int[] apprentAge)
+            {
+                Console.WriteLine($"There are {apprentAge.Length} students");
+                Console.WriteLine($"The youngest student is {apprentAge.Min()}");
+                Console.WriteLine($"The oldest student is {apprentAge.Max()}");
+                Console.WriteLine($"The average age of all students {apprentAge.Average():F2}");
+                for (int i = 0; i < apprentAge.Length; i++)
+                {
+                    Console.WriteLine($"Student #{i + 1} '{firstName[i]} {lastName[i]}' aged '{apprentAge[i]}'");
+
+                }
+            }
+        }
+
+        static void ComeBackToDaySeven()
+        { 
+        string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\";
+        string csvFilePath = desktopPath + @"SampleCSV.csv";
+        string[] importedCSV = File.Exists(csvFilePath) ? File.ReadAllLines(csvFilePath) : new string[] { };
+        int totalLines = importedCSV.Length;
+        bool isHeader=true;
+
+        List<int> ages = new List<int>();
+        List<string> names = new List<string>();
+
+        for (int i = 1; i < totalLines; i++)
+        {
+            string[] dataFields = importedCSV[i].Split(",");
+            ages.Add(int.Parse(dataFields[2]));
+        }
+        } // comeBackToDaySeven()
+    } // DayEight()
+
 
     static void DaySeven()
     {
@@ -87,29 +172,34 @@ internal class Program
             int totalAge = 0;
             
             int skipNumberLines = 0;
-            bool skipHeaders = true;
-            if (skipHeaders) {
+            bool isHeader = false;
+            if (isHeader) {
                 skipNumberLines = 1;
             }
             // never use CristianName or SurName, always use FirstName and LastName.
             string[] studentNames = new string[totalLines];
             int[] ages = new int[totalLines];
+            string rawHeader;
 
             foreach (string line in importedCSV.Skip(skipNumberLines))
             {
                 Console.WriteLine($"Line #{linecount + 1}: {line}");
                 string[] dataFields = line.Split(",");
                 //Console.WriteLine(dataFields.Length);
-                if (skipHeaders)
+                if (!isHeader)
                 {
                     int age = Convert.ToInt32(dataFields[2]);
                     totalAge += age;
                     studentNames[linecount] = dataFields[0] + " " + dataFields[1];
                     ages[linecount] = age;
                 }
+                else
+                {
+                    if (linecount == 0)
+                        rawHeader = line;
+                    isHeader = false;
+                }
                 linecount++;
-
-
             }
 
             for (int i=0;  i < totalLines; i++)
